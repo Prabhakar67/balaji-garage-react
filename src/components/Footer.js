82c4e4cde84ae556dc3c7e7fe6d1410a11873033
prabhakar <prabhakaraher13@gmail.com>
2022-12-23 00:45:32 +0530

    init

---
+++ src/components/Footer.js
@@ -0,0 +1,32 @@
+import React from "react"
+
+
+const Footer = () => {
+    return (
+        <>
+            <footer className="footer-distributed">
+
+                <div className="footer-left">
+
+                    <p className="footer-links">
+                        <a href="#">Home</a>
+
+                        <a href="#">About</a>
+
+                        <a href="#">Services</a>
+
+                        <a href="#">Faq</a>
+
+                        <a href="#">Contact</a>
+                    </p>
+
+                    <p>Cyncly &copy; 2022</p>
+                </div>
+
+            </footer>
+        </>
+
+    )
+}
+
+export default Footer
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Navbar = () => {
+    return (
+        <ul className="nav">
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+      </ul>
+    )
+}
+
+export default Navbar
