0e722ceb5549c5123d7a6e1746075d93ae5a2f01
prabhakar <prabhakaraher13@gmail.com>
2022-12-30 19:28:22 +0530

    bike added

--- src/subcomponents/Headings.js
+++ src/subcomponents/Headings.js
@@ -6,7 +6,7 @@ function Headings() {
             <div class="heading">
                 Complete Two Wheeler Service Solution
             </div>
-            <h3 class="subheading">service your 2 wheeler from the place of your choice in Pune</h3>
+            <h3 class="subheading">service your bike from the place of your choice in Pune</h3>
         </div>
     );
 }
